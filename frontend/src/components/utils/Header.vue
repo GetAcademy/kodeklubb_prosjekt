0a6463707c6aa077799cef1890b751c0a9942cca
krigjo25 <krigjo25@gmail.com>
2026-01-27 13:59:43 +0100

    revert: ⏪️ Reverted Changes, Added window.location.href.

--- frontend/src/components/utils/Header.vue
+++ frontend/src/components/utils/Header.vue
@@ -1,19 +1,19 @@
 <template>
 
-    <button v-if="!isLoggedIn" @click="loginWithDiscord()" class = 'discord-btn'> This is a login button for discord</button>
+    <button v-if="!isLoggedIn" @click="loginWithDiscord" class = 'discord-btn'> This is a login button for discord</button>
 
 </template>
 <script setup lang="ts">
 
     // --- Importing Dependencies
     import axios from 'axios';
-import { computed } from 'vue';
-    import { useRoute, useRouter } from 'vue-router';
+    import { computed } from 'vue';
 
+
+    const meta = import.meta.env;
     // --- Discord client API Logic
-    const apiUrl =  import.meta.env.VITE_DISCORD_API_LOGIN;
+    const apiUrl =  meta.VITE_DISCORD_API_LOGIN;
     const isLoggedIn = computed(() => false);
-
 
     async function discordCallBack()
     {
@@ -27,29 +27,16 @@ import { computed } from 'vue';
     }
     async function loginWithDiscord()
     {
-        const meta = import.meta.env;
+        
         const BASE_API = meta.VITE_C_SERVER;
         const LOGIN_API = `${BASE_API}${meta.VITE_LOGIN_API}`;
-        //const API_CLIENT = `${BASE_API}`.join(import.meta.env.VITE_LOGIN_API);
 
-        const apiUrl =  meta.VITE_DISCORD_API_LOGIN;
-        
-
-        const payload =
-        {
-            header:
-            {
-                clientId: meta.VITE_DISCORD_CLIENT_ID,
-                clientSecret: meta.VITE_DISCORD_CLIENT_SECRET
-            }
-        }
+        window.location.href = LOGIN_API;
 
         try {
-            const response = await axios.get(LOGIN_API);
-            //discordCallBack();
-            isLoggedIn.value = true
+            
+            discordCallBack();
 
-            //console.log(LOGIN_API, apiUrl);
         } catch (e){
             console.error('Innlogging feilet :', e);
         }
