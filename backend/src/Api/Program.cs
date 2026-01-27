6ba07c0109067fb61bfafe97373199fa98734087
martininn <martininnvaer0103@gmail.com>
2026-01-27 11:33:48 +0100

    chore(API): Use Cors correctly AFTER building

--- backend/src/Api/Program.cs
+++ backend/src/Api/Program.cs
@@ -30,10 +30,10 @@ builder.Services.AddCors(options =>
             .AllowCredentials();
     });
 });
-app.UseCors();
 
 var app = builder.Build();
 
+app.UseCors("DevCors");
 app.UseHttpsRedirection();
 
 // Configure the HTTP request pipeline.
