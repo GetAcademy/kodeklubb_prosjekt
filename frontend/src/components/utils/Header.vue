94a07978bdef983943a650924fc2da387642f07e
krigjo25 <krigjo25@gmail.com>
2026-01-27 13:16:01 +0100

    refactor: Initiating Post Request to the server.

--- frontend/src/components/utils/Header.vue
+++ frontend/src/components/utils/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <button @click="loginWithDiscord" class = 'discord-btn'> This is a login button for discord</button>
+    <button @click="loginWithDiscord()" class = 'discord-btn'> This is a login button for discord</button>
 </template>
 <script setup lang="ts">
 
@@ -8,11 +8,8 @@
     import { useRoute, useRouter } from 'vue-router';
 
     // --- Discord client API Logic
-    const apiUrl =  import.meta.env.DISCORD_API_LOGIN;
-    const clientID = import.meta.env.DISCORD_CLIENT_ID;
-    const clientSecret = import.meta.env.DISCORD_CLIENT_SECRET;
+    const apiUrl =  import.meta.env.VITE_DISCORD_API_LOGIN;
 
-    const route = useRoute();
     const router = useRouter();
 
 
@@ -20,23 +17,41 @@
     {
         try{
             const response = await axios.get('/auth/discord/callback');
+            return response;
         } catch (e) {console.log(e)}
     }
     async function loginWithDiscord()
     {
-        const code = route.query.code;
+        const meta = import.meta.env;
+        const BASE_API = meta.C_SERVER;
+        //const API_CLIENT = `${BASE_API}`.join(import.meta.env.VITE_LOGIN_API);
 
-        if (code)
+        const apiUrl =  import.meta.env.VITE_DISCORD_API_LOGIN;
+        const clientID = import.meta.env.VITE_DISCORD_CLIENT_ID;
+        const clientSecret = import.meta.env.VITE_DISCORD_CLIENT_SECRET;
+        
+
+        const payload =
         {
-            try {
-                const response = await axios.post('/auth/discord/login', {code});
-                localStorage.setItem('user_token', response.data.token);
-                router.push('/dashboard');
-                discordCallBack();
-            } catch (e){
-                console.error('Innlogging feilet', e);
+            header:
+            {
+                clientId:clientID,
+                clientSecret: clientSecret
             }
+            
         }
+
+        try {
+            const response = await axios.post("http://localhost:5154/auth/discord/login", payload);
+            discordCallBack();
+            localStorage.setItem('user_token', response.data.token);
+            router.push('/dashboard');
+
+            console.log("test")
+        } catch (e){
+            console.error('Innlogging feilet', e);
+        }
+
     }
     // -- Debugging Logic
     console.log("connecting to :", apiUrl);
