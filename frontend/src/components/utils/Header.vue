509c34f62643a99e81d0d2646eea98b39e696b7a
krigjo25 <krigjo25@gmail.com>
2026-01-27 13:43:07 +0100

    refactor(Header): ♻️ Updated Api logic

--- frontend/src/components/utils/Header.vue
+++ frontend/src/components/utils/Header.vue
@@ -1,55 +1,57 @@
 <template>
-    <button @click="loginWithDiscord()" class = 'discord-btn'> This is a login button for discord</button>
+
+    <button v-if="!isLoggedIn" @click="loginWithDiscord()" class = 'discord-btn'> This is a login button for discord</button>
+
 </template>
 <script setup lang="ts">
 
     // --- Importing Dependencies
     import axios from 'axios';
+import { computed } from 'vue';
     import { useRoute, useRouter } from 'vue-router';
 
     // --- Discord client API Logic
     const apiUrl =  import.meta.env.VITE_DISCORD_API_LOGIN;
-
-    const router = useRouter();
+    const isLoggedIn = computed(() => false);
 
 
     async function discordCallBack()
     {
         try{
             const response = await axios.get('/auth/discord/callback');
-            return response;
-        } catch (e) {console.log(e)}
+
+            localStorage.setItem('user_token', response.data.token);
+            console.log(response)
+
+        } catch (e) {console.error(e)}
     }
     async function loginWithDiscord()
     {
         const meta = import.meta.env;
-        const BASE_API = meta.C_SERVER;
+        const BASE_API = meta.VITE_C_SERVER;
+        const LOGIN_API = `${BASE_API}${meta.VITE_LOGIN_API}`;
         //const API_CLIENT = `${BASE_API}`.join(import.meta.env.VITE_LOGIN_API);
 
-        const apiUrl =  import.meta.env.VITE_DISCORD_API_LOGIN;
-        const clientID = import.meta.env.VITE_DISCORD_CLIENT_ID;
-        const clientSecret = import.meta.env.VITE_DISCORD_CLIENT_SECRET;
+        const apiUrl =  meta.VITE_DISCORD_API_LOGIN;
         
 
         const payload =
         {
             header:
             {
-                clientId:clientID,
-                clientSecret: clientSecret
+                clientId: meta.VITE_DISCORD_CLIENT_ID,
+                clientSecret: meta.VITE_DISCORD_CLIENT_SECRET
             }
-            
         }
 
         try {
-            const response = await axios.post("http://localhost:5154/auth/discord/login", payload);
-            discordCallBack();
-            localStorage.setItem('user_token', response.data.token);
-            router.push('/dashboard');
+            const response = await axios.get(LOGIN_API);
+            //discordCallBack();
+            isLoggedIn.value = true
 
-            console.log("test")
+            //console.log(LOGIN_API, apiUrl);
         } catch (e){
-            console.error('Innlogging feilet', e);
+            console.error('Innlogging feilet :', e);
         }
 
     }
