55fed1767d4393d922bd3aa0dc5cdfc74bce9774
krigjo25 <krigjo25@gmail.com>
2026-01-29 14:42:05 +0100

    feat(UI): ✨ Introducing Bar Component for Profiles.

---
+++ frontend/src/components/profile/Bar.vue
@@ -0,0 +1,23 @@
+<template>
+    <aside>
+        <section v-if="data">
+            <picture>
+            </picture>
+        </section>
+    </aside>
+</template>
+
+<script lang="ts" setup>
+
+    //  --- Importing Deps, Types
+    import { computed } from 'vue';
+
+    interface BarProps
+    {
+        data: Record<string,string>
+    }
+    const props = defineProps<BarProps>();
+
+    const data = computed(() => props.data);
+    
+</script>
