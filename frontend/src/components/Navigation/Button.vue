1fd068e6df65b182fa3882470a22e2144f65367a
krigjo25 <krigjo25@gmail.com>
2026-01-29 14:23:50 +0100

    feat(UI): ✨ Add Disabled State -> Button Component.

--- frontend/src/components/Navigation/Button.vue
+++ frontend/src/components/Navigation/Button.vue
@@ -1,11 +1,18 @@
 <template>
-    <button @click="data.action()"> {{ data.label }}</button>
+    <button
+        @click="data.action()"
+        :type="data.type ? data.type : 'button'"
+        :disabled="data.disabled">
+        
+        {{ data.label }}
+
+    </button>
 </template>
 
 <script lang="ts" setup>
 
     import { computed } from 'vue';
-    import type { ButtonProps } from '@/types/navigation/button';
+    import type { ButtonProps } from '@/types/navigation/buttons';
 
     const props = defineProps<ButtonProps>();
     const data = computed(() => props.data);
--- frontend/src/types/navigation/buttons.d.ts
+++ frontend/src/types/navigation/buttons.d.ts
@@ -3,6 +3,7 @@ export interface ButtonItem
     type: string,
     label: string,
     action: () => void,
+    disabled?: boolean;
 }
 
 export interface ButtonProps { data: ButtonItem}
