7663fb5bc75d2a51f59335c3ce7779b8c2535d4b
krigjo25 <krigjo25@gmail.com>
2026-01-29 14:17:59 +0100

    refactor(Component): 🚚 Relocate Dashboard Component to Utils & Relocate Dashboard logic into Index.vue

---
+++ frontend/src/components/utils/Dashboard.vue
@@ -0,0 +1,33 @@
+<template>
+    <h2>Dashboard</h2>
+    <p> 
+        Velkommen til Kodeklubben, <b>{{ data.name }}</b> - <b>{{ data.email }}</b>
+    </p>
+    {{ data }}
+
+    <ProfileBar :data="data"/>
+
+    <section>
+        <h2> Dine Teams </h2>
+    </section>
+
+    <section>
+        <h2> Discover new Teams</h2>
+    </section>
+</template>
+
+<script lang="ts" setup>
+
+    // --- Importing Dependencies & Types
+    import { computed } from 'vue';
+
+    // --- Props Definition Logic
+    interface DashboardProps
+    {
+        data: any;
+    }
+    const props = defineProps<DashboardProps>();
+    const data = computed(() => props.data);
+    console.log(data.value)
+
+</script>
