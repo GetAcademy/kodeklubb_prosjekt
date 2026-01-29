f2b3f0f6210ce05601ae8b831ec06af8aeb6caca
krigjo25 <krigjo25@gmail.com>
2026-01-29 13:03:00 +0100

    feat(Route): 🔧 Update Idex Router Logic for new View Architecture.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,18 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const views: string = '../views';
-
 const requiredAuthorization: Array<any> =
 [
-  { path: "/profile", name : "profile", component: () => import(`${views}/Profile.vue`) },//, meta: {requiresAuth: true} }
-  { path: "/dashboard", name : "dashboard", component: () => import(`${views}/Dashboard.vue`) }//, meta: {requiresAuth: true} }
+  { path: "/profile", name : "profile", component: () => import(`../views/Profile.vue`) },//, meta: {requiresAuth: true} }
+  { path: "/dashboard", name : "dashboard", component: () => import(`../views/Index.vue`) }//, meta: {requiresAuth: true} }
   
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/", name : "index", component: () => import(`${views}/indexView.vue`) },
+    { path: "/", name : "index", component: () => import(`../views/Index.vue`) },
+    { path: "/logout", name : "logout", component: () => import(`../views/Index.vue`) },
     ...requiredAuthorization
   ],
 })
