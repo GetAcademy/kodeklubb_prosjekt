b2ad1a1d7e6e08f0af14ce8f36d0906d19fdf04b
krigjo25 <krigjo25@gmail.com>
2026-01-30 13:57:42 +0100

    chore(Types): 🎨 Improved the structure of Types.

--- frontend/src/types/navigation/navigation.d.ts
+++ frontend/src/types/navigation/navigation.d.ts
@@ -1,10 +1,10 @@
 import type { Anchor, RouteItem } from "./anchor";
+import type { ButtonItem } from "./buttons";
 
 export interface NavigationProp
 {
-    type: string;
     totalPage?: number;
     activePage?: number;
-    data: RouteItem | Anchor[];
+    data: RouteItem | Anchor[] | ButtonItem[];
     cls?: Array<string | string[] | Array<string | string[]>>;
 }
