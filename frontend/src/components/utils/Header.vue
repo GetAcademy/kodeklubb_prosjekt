a98121f192a2f355008a4082f38760cb0f235e30
krigjo25 <krigjo25@gmail.com>
2026-02-02 16:10:05 +0100

    refactor(Route): ♻️ Replaced Static Route Definition with Dynamic auto-discovery.

--- frontend/src/components/utils/Header.vue
+++ frontend/src/components/utils/Header.vue
@@ -3,16 +3,19 @@
     <NavigationMenu v-if="isAuthenticated" :data="authMenu"/>
     <NavigationMenu v-else :data="menu"/>
     <h1> GET - Kode Klubb</h1>
-    
+    {{ menu }}
 
 </template>
 <script setup lang="ts">
 
     // --- Importing Dependencies & Types
-    import { storeToRefs } from 'pinia';    
+    import { computed } from 'vue';
+    import { storeToRefs } from 'pinia'; 
+    import { useRouter } from 'vue-router';  
     import { useAuthStore } from '@/stores/authStore';
 
     // --- State Management
+    const router = useRouter()
     const authStore = useAuthStore();
     const { isAuthenticated } = storeToRefs(authStore);
 
@@ -20,18 +23,38 @@
     const BASE_API = meta.VITE_BASE_API;
     const discordAPI = `${BASE_API}${meta.VITE_LOGIN_API}`;
 
-    const menu = 
-    [
-        { type: 'button', label:"Logg inn with Discord", cls:"discord-btn", action: () => loginDiscord()}
-    ]
+    const authMenu = computed(() =>
+    {
+        return router.getRoutes().filter(route => route.meta?.requiresAuth).map(route =>
+        {
+            switch (route.path)
+            {
+                case '/': return { type: 'router', label: 'Dashboard' };
+                case '/logout': return { type: 'button', label: 'Logg ut', action: () => handleLogout(), icon: 'logout' };
+                default : return { type: 'router', path: route.path, label: route.name || route.path,};
+            }
+        });
+    });
 
-    const authMenu =
-    [
-        { type: 'router', path:"/profile", label:"Min Side"},
-        { type: 'button', path:"/logout", label:"Logg deg ut", cls:"logout-btn", action: () => handleLogout() }
-    ];
- 
-    async function  loginDiscord() { window.location.href = discordAPI;}
-    function handleLogout () { authStore.logout();}
+    const menu = computed(() =>
+    {
+        return router.getRoutes().filter(route => !route.meta?.requiresAuth).map(route => {
+
+            if (route.path === '/') { return { type: 'button', label:"Discord login", cls:"discord-btn", action: () => loginDiscord()}}
+            // Standard rute (vanlig lenke)
+            return {
+                type: 'router',
+                path: route.path,
+                label: route.name || route.path,
+            };
+    });
+    });
+
+    async function  loginDiscord(){window.location.href = discordAPI;}
+    async function  handleLogout ()
+    { 
+        await authStore.logout();
+        await router.replace('/');
+    }
 
 </script>
