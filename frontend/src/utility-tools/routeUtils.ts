5dc43ad2589e15ec02b1c72cecf608077a18eac0
krigjo25 <krigjo25@gmail.com>
2026-02-06 10:41:10 +0100

    fix(Route): 🐛 Ensures URL Query Parameters are Cleared on Route Change.

--- frontend/src/utility-tools/routeUtils.ts
+++
@@ -1,8 +0,0 @@
-export function sanitizeUrlParams (params: string[]) : void
-    {
-        if (!params) return;
-        let hasChanged: boolean = false;
-        const url = new URL(window.location.href)
-        params.forEach(param => { url.searchParams.delete(param); hasChanged = true; });
-        if (hasChanged){window.history.replaceState({}, '/', url.pathname.toString());}
-    }
