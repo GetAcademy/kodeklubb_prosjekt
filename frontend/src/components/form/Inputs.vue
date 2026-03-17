f3d143d5ba5b98c5fdae0bf6277588b700bfe0ec
Husseinabdulameer11 <husseinabdulameer11@gmail.com>
2026-03-17 08:40:22 +0100

    Clean up template bindings in Inputs.vue for improved readability

--- frontend/src/components/form/Inputs.vue
+++ frontend/src/components/form/Inputs.vue
@@ -1,26 +1,5 @@
 <template>
-        :id="data.id"
-        :class="cls[1]"
-        :name="data.name"
-        :placeholder="!!data.placeholder ? data.placeholder : ''"
-        :min="data.type =='range' ? !!data.rangeMin? data.rangeMin : 0 : ''"
-        :step="data.type =='range' ? 1 : 0"
-        :max="data.type =='range' ? !!data.rangeMax? data.rangeMax : 100 : ''"
-        :value="!!data.value ? data.value : ''"
-        :type="data.type ? data.type : 'text'"
-        :size="!!data.size ? data.size : '30'"
-        :width="!!data.width ? data.width : ''"
-        :height="!!data.height ? data.height : ''"
-        :pattern="!!data.pattern ? data.pattern : ''"
-        :readonly="!!data.readonly ? data.readonly : false"
-        :required="!!data.required ? data.required : false"
-        :disabled="!!data.disabled ? data.disabled : false"
-        :maxlength="!!data.maxlength ? data.maxlength : ''"
-        :minlength="!!data.minlength ? data.minlength : ''"
-        :autofocus="!!data.autofocus ? data.autofocus : false"
-        :multiple="!!data.multiple && data.type == 'file' ? data.multiple : false"
-        @input="emit('update:modelValue', $event.target.value)"
-    />
+    <!-- Removed stray template bindings. Only valid elements below. -->
     <label v-if="data.label" :for="data.name" :class="cls[0]">{{ data.label || data.name }}</label>
     <input
         :id="data.id"
