5a58b518781ef7ca4fdbc6f453c9dc7f99d377e6
swatison173 <swati@getacademy.no>
2026-08-17 11:55:20 +0200

    Update TagsSql.cs

--- backend/src/Persistence/Sql/TagsSql.cs
+++ backend/src/Persistence/Sql/TagsSql.cs
@@ -3,4 +3,6 @@ namespace Persistence;
 public static class TagsSql
 {
     public static string GetAll() => SqlLoader.Load("Queries/PredefinedTags_GetAll.sql");
+
+    public static string CheckExists() => SqlLoader.Load("Queries/PredefinedTags_CheckExists.sql");
 }
